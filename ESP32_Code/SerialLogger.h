fe1a394129be7cabd47218f508739e8ae4799198
salehaahmed14 <113420201+salehaahmed14@users.noreply.github.com>
2024-12-20 13:31:55 +0500

    serial logging code.

---
+++ ESP32_Code/SerialLogger.h
@@ -0,0 +1,20 @@
+#ifndef SERIALLOGGER_H
+#define SERIALLOGGER_H
+
+#include <Arduino.h>
+
+#ifndef SERIAL_LOGGER_BAUD_RATE
+#define SERIAL_LOGGER_BAUD_RATE 115200
+#endif
+
+class SerialLogger
+{
+public:
+  SerialLogger();
+  void Info(String message);
+  void Error(String message);
+};
+
+extern SerialLogger Logger;
+
+#endif // SERIALLOGGER_H
